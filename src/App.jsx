93b822e67ac8dbba3afb4c6d9e5a989fc46bf596
Tri Ngo <tringo831@gmail.com>
2024-11-25 21:20:07 -0800

    Use data loaders to fetch data and display on a single job page

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage/>} />
       <Route path='/jobs' index element={<JobsPage/>} />
+      <Route path='/jobs/:id' index element={<JobPage/>} loader={jobLoader} />
       <Route path='*' index element={<NotFoundPage/>} />
     </Route>
   )
